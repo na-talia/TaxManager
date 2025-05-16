54f210a3b1c9baf3b1a5560638fcbe83dcdcbb12
na-talia <ntsh_89@ukr.net>
2025-05-16 20:03:13 +0200

    feat: add hardcoded TaxRecord list and print it

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,36 @@
-var tax = new TaxRecord
+var taxList = new List<TaxRecord>
 {
-    Municipality = "Copenhagen",
-    FromDate = new DateOnly(2025, 01, 01),
-    ToDate = new DateOnly(2025, 12, 31),
-    TaxRate = 0.2,
-    TaxType = "Yearly"
+    new TaxRecord {
+        Municipality = "Copenhagen",
+        FromDate = new DateOnly(2025, 01, 01),
+        ToDate = new DateOnly(2025, 12, 31),
+        TaxRate = 0.2,
+        TaxType = "Yearly"
+    },
+    new TaxRecord {
+        Municipality = "Copenhagen",
+        FromDate = new DateOnly(2025, 05, 01),
+        ToDate = new DateOnly(2025, 05, 31),
+        TaxRate = 0.4,
+        TaxType = "Monthly"
+    },
+    new TaxRecord {
+        Municipality = "Copenhagen",
+        FromDate = new DateOnly(2025, 01, 01),
+        ToDate = new DateOnly(2025, 01, 01),
+        TaxRate = 0.1,
+        TaxType = "Daily"
+    },
+    new TaxRecord {
+        Municipality = "Copenhagen",
+        FromDate = new DateOnly(2025, 12, 25),
+        ToDate = new DateOnly(2025, 12, 25),
+        TaxRate = 0.1,
+        TaxType = "Daily"
+    }
 };
 
-Console.WriteLine($"{tax.Municipality} from {tax.FromDate} to {tax.ToDate} has a {tax.TaxType} tax of {tax.TaxRate}.");
+foreach (var tax in taxList)
+{
+    Console.WriteLine($"{tax.Municipality} from {tax.FromDate} to {tax.ToDate} has a {tax.TaxType} tax of {tax.TaxRate}.");
+}
